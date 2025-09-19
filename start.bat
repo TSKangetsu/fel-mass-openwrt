a2141edee516685e48bd6f773f5ea6ce9fed8a78
TSKangetsy <x403070567@live.com>
2025-09-19 15:21:30 +0800

    bootable update

--- start.bat
+++ start.bat
@@ -1,8 +1,1 @@
-@echo off
-echo "Connect device in FEL mode and press <Enter>"
-pause
-
-set BOARD=h3
-
-win32\sunxi-fel.exe -p uboot %BOARD%\u-boot-sunxi-with-spl.bin write 0x42000000 %BOARD%\zImage write 0x43000000 %BOARD%\script.bin write 0x43300000 %BOARD%\uInitrd write 0x43100000 %BOARD%\boot.scr
-pause
+sunxi-fel.exe -p uboot u-boot-sunxi-with-spl.bin write 0x45000000 openwrt-sunxi-cortexa7-friendlyarm_nanopi-neo-air-initramfs-kernel.bin write 0x48000000 dtb write 0x49000000 boot.scr
