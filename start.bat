9c4cfe21cb7d07681fa5a55ba1366e56f8d4cb22
TSKangetsu <x403070567@live.com>
2025-09-19 21:47:08 +0800

    complete

--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
 sunxi-fel.exe -p uboot u-boot-sunxi-with-spl.bin write 0x45000000 openwrt-sunxi-cortexa7-friendlyarm_nanopi-neo-air-initramfs-kernel.bin write 0x48000000 dtb write 0x49000000 boot.scr
+@rem sunxi-fel.exe -p uboot u-boot-sunxi-with-spl.bin 
